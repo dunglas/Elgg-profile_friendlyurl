7d37e941375aca24cb6298c6f7ec3e782cbdb3cd
Kévin Dunglas <dunglas@macbook-de-kevin-dunglas-2.local>
2010-01-14 01:09:48 +0100

    first commit

---
+++ start.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Elgg profile_friendly_url plugin
+ *
+ * @package Elggprofile_friendly_url
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author Kévin Dunglas <dunglas@gmail.com>
+ * @copyright Kévin Dunglas - 2010
+ */
+
+/**
+ * poll extended initialization
+ *
+ * Register css extensions, contentes view for groups, widgets and event handlers
+ */
+function profile_friendly_url_init() {
+}
+?>
---
+++ views/default/settings/profile_friendlyurl/edit.php
@@ -0,0 +1,7 @@
+<?php
+?>
+<p>
+	<?php echo elgg_echo('profile_friendlyurl:domain'); ?>
+	
+	<input type="text" name="params[domain]" value="<?php echo $vars['entity']->domain ?>" />
+</p>
